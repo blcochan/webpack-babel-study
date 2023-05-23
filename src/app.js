b71de3ee9724f57656f2284152b2b6c2d8942455
박종찬 <chanii8@naver.com>
2023-05-23 09:25:03 +0900

    practice: loader

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 import { math } from "./math.js";
+import "./app.css";
+import favicon from "./images/favicon.png";
+import codit from "./images/share_codit_search_en.png";
 
 console.log(math.sum(1, 2));
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.innerHTML = `
+        <img src="${favicon}" /><br />
+        <img src="${codit}" />
+    `;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
   },
   output: {
     path: path.resolve("./dist"),
-    filename: "[name].js", // name에 entry 키 값이 들어옴 (main)
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: path.resolve("./my-webpack-loader.js") },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader",
+        options: {
+          name: "[name].[ext]?[hash]",
+          limit: 10000,
+        },
+      },
+    ],
   },
 };
