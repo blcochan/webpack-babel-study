5d338b3f4cb927e83b796383650e53e54cfad000
박종찬 <chanii8@naver.com>
2023-06-19 17:15:50 +0900

    practice: plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -12,7 +17,15 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, use: path.resolve("./my-webpack-loader.js") },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.css$/,
+        use: [
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader
+            : "style-loader",
+          "css-loader",
+        ],
+      },
       {
         test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: "url-loader",
@@ -23,4 +36,18 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `
+        Build Time: ${new Date().toLocaleString()}
+        Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+        Author: ${childProcess.execSync("git config user.name")}
+      `,
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
